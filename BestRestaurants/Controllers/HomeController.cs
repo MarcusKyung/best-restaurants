3573eb4bff9f89bd5a0cfacded85944763648037
MarcusKyung <Kyungmj@gmail.com>
2023-05-24 14:12:11 -0700

    Add splash page and images to index/footer
    Co-authored-by: Max Betich <MaxBetich@users.noreply.github.com>

--- BestRestaurants/Controllers/HomeController.cs
+++ BestRestaurants/Controllers/HomeController.cs
@@ -1,14 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace BestRestaurants.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly BestRestaurantsContext _db;
+
+      public HomeController(BestRestaurantsContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        Restaurant[] restaurants = _db.Restaurants.ToArray();
+        Cuisine[] cuisines = _db.Cuisines.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("restaurants", restaurants);
+        model.Add("cuisines", cuisines);
+        return View(model);
       }
 
     }
